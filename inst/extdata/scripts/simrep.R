e8e8895fc7ef229e7fb98f3f686927ab0249d672
John Robinson <jdrob@msu.edu>
2019-06-14 16:00:03 -0400

    Add files via upload

--- inst/extdata/scripts/simrep.R
+++ inst/extdata/scripts/simrep.R
@@ -6,6 +6,11 @@
 
 library(holoSimCell)
 
+#Run these in $TMPDIR on the cluster so they don't step on one another
+codedir = "/mnt/research/TIMBER/spatsimsEW/code"
+simdir = system("echo $TMPDIR", intern = TRUE)
+outdir = "/mnt/research/TIMBER/spatsimsEW/OUT"
+
 #Set the row of the scenarios table you're working with 
 #and the number of replicates using command line arguments
 #Usage: Rscript sim_spatstat.R 1 1 100
@@ -20,8 +25,8 @@ if(length(args) == 0){
 }
 
 #Read in the scenarios file (alternative parameter combinations for spatial stat evaluation)
-setwd("/mnt/research/TIMBER/spatsimsEW/code")
-#setwd("~/Google Drive/MSU/TIMBER/Weise_Sims_2019/dist")
+setwd(codedir)
+
 parmsets <- read.csv("scenarios.csv", header = TRUE)
 parms <- parmsets[i,]
 
@@ -43,15 +48,13 @@ ncol = 15
 
 sample_pops <- seq(1, nrow*ncol, 11)
 sample_n <- rep(25,length(sample_pops))
-#sample_pops <- sort(sample(c(1:225),25,replace=FALSE)) 
 
-
-setwd("/mnt/research/TIMBER/spatsimsEW/OUT/")
-#setwd("~/Desktop/SIMS")
-
+#Loop over the replicates 
 for(repl in 1:nreps) {
 
-pops <- getpophist.cells(h=nrow*ncol,          ##demography, num habitats - product of xdim & ydim
+    setwd(simdir)
+
+    pops <- getpophist.cells(h=nrow*ncol,  ##demography, num habitats - product of xdim & ydim
                          xdim=ncol,        ##num cols - fixed at 15
                          ydim=nrow,        ##num rows - fixed at 15
                          maxtime=parms$texp/parms$G,  ##num time clicks to simulate (yrs, decades cents?) (call em decades here)  #!# reduced this a bit for testing, from 12000
@@ -76,13 +79,13 @@ pops <- getpophist.cells(h=nrow*ncol,          ##demography, num habitats - prod
                          hab_suit = NULL
                          )
 
-parms_out <- c(parms, loc_parms2, preLGMparms)
+    parms_out <- c(parms, loc_parms2, preLGMparms)
 
-#Run fastsimcoal in a while loop
-#Only stops when loc_parms$nloci SNPs or more are in the output
-fscout <- NULL
-while(is.null(fscout)) {
-    tmp <- runFSC_step(popsobj = pops,  		    #Pass the 'pops' object from getpophist.cells()
+    #Run fastsimcoal in a while loop
+    #Only stops when loc_parms$nloci SNPs or more are in the output
+    fscout <- NULL
+    while(is.null(fscout)) {
+        tmp <- runFSC_step(popsobj = pops,  		#Pass the 'pops' object from getpophist.cells()
 					preLGMparms = preLGMparms,	    #This has parms for the refuge, preLGM size and timing
 					sample_pops = sample_pops,	    #The IDs of sampled populations
 					sample_n = sample_n,		    #The sample size per population
@@ -93,53 +96,54 @@ while(is.null(fscout)) {
 					loc_parms = loc_parms2,		    #Vector of locus parameters
 					found_Ne = parms$foundNe)		#Founding size 
 
-    SNPnum <- get.gSum(tmp)
-    message(paste("Coalescent simulation complete:", SNPnum$nvar, "variable sites"))
+        SNPnum <- get.gSum(tmp)
+        message(paste("Coalescent simulation complete:", SNPnum$nvar, "variable sites"))
 
-    #This is a check to see if there are enough variable sites in the dataset...
-    if(SNPnum$nvar > loc_parms$nloci) {
-        variableloci <- which(SNPnum$nall == 2)
-        keeploci <- sample(variableloci,loc_parms$nloci,replace = FALSE)
-        fscout <- tmp[,keeploci]
-        rm(tmp)
-    } else if(SNPnum$nvar == loc_parms$nloci) {
-        fscout <- tmp
-        rm(tmp)
-    } else {
-        message("too few SNPs, doubling number of simulated loci and trying again")
-        loc_parms2$nloci <- loc_parms2$nloci*2
-        rm(tmp)
+        #This is a check to see if there are enough variable sites in the dataset...
+        if(SNPnum$nvar > loc_parms$nloci) {
+            variableloci <- which(SNPnum$nall == 2)
+            keeploci <- sample(variableloci,loc_parms$nloci,replace = FALSE)
+            fscout <- tmp[,keeploci]
+            rm(tmp)
+        } else if(SNPnum$nvar == loc_parms$nloci) {
+            fscout <- tmp
+            rm(tmp)
+        } else {
+            message("too few SNPs, doubling number of simulated loci and trying again")
+            loc_parms2$nloci <- loc_parms2$nloci*2
+            rm(tmp)
+        }
     }
-}
 
-#Naming the strata - uses grid cell # with left-padding to make all the same length
-#Treated as a factor through, do not index with this padded value
-fscout <- nameStrat(fscout = fscout, pops = pops, sample_pops = sample_pops, sample_n = sample_n)
+    #Naming the strata - uses grid cell # with left-padding to make all the same length
+    #Treated as a factor through, do not index with this padded value
+    fscout <- nameStrat(fscout = fscout, pops = pops, sample_pops = sample_pops, sample_n = sample_n)
 
-#Build popDF for stat calculation
-popDF <- data.frame(id = unique(fscout@data$strata),
+    #Build popDF for stat calculation
+    popDF <- data.frame(id = unique(fscout@data$strata),
                                 grid.cell = sample_pops,
                                 sample.size = sample_n,
                                 col = pops$pophist$col[sample_pops],
                                 row = pops$pophist$row[sample_pops])
 
 
-stats_out <- holoStats(out = fscout, 
-                        popDF = popDF,
-                        extent = c(pops$struct["xdim"], pops$struct["ydim"]), 
-                        cores = 1) 
+    stats_out <- holoStats(out = fscout, 
+                            popDF = popDF,
+                            extent = c(pops$struct["xdim"], pops$struct["ydim"]), 
+                            cores = 1) 
 
-#!# IS THIS OUTPUT IN THE FORMAT THAT WE WANT??
-all_out <- c(rep = repl, date = date(), parms_out, stats_out)
-all_out <- all_out[c(3,1,2,4:length(all_out))]
-all_out$refs <- paste(eval(parse(text=as.character(parms$refs))), collapse = ".")
+    #!# IS THIS OUTPUT IN THE FORMAT THAT WE WANT??
+    all_out <- c(rep = repl, date = date(), parms_out, stats_out)
+    all_out <- all_out[c(3,1,2,4:length(all_out))]
+    all_out$refs <- paste(eval(parse(text=as.character(parms$refs))), collapse = ".")
 
-#Write a file, if none exists, or append to file, if present
-if(!file.exists(paste0("SpatStats_parmset-",i,".csv"))) {
-    write.table(all_out, paste0("SpatStats_parmset-",i,".csv"), sep = ",", quote = FALSE, row.names = FALSE)
-} else {
-    write.table(all_out, paste0("SpatStats_parmset-",i,".csv"), quote = FALSE, row.names = FALSE, sep=",", append = TRUE, col.names = FALSE)
-}
+    #Write a file, if none exists, or append to file, if present
+    setwd(outdir)
 
+    if(!file.exists(paste0("SpatStats_parmset-",i,".csv"))) {
+        write.table(all_out, paste0("SpatStats_parmset-",i,".csv"), sep = ",", quote = FALSE, row.names = FALSE)
+    } else {
+        write.table(all_out, paste0("SpatStats_parmset-",i,".csv"), quote = FALSE, row.names = FALSE, sep=",", append = TRUE, col.names = FALSE)
+    }
 
 }
