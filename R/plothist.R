6cf30293652a2af9ae7af3d76e1224c89d794bbe
allan strand <stranda@gmail.com>
2019-07-08 11:49:36 -0400

    modified the plot routine

--- R/plothist.R
+++ R/plothist.R
@@ -1,10 +1,62 @@
 #'
 #' plot the population history
+#' @param ph population history object (direct output from gepophist.cells)
+#' @export
+#' 
+plothist <- function(ph)
+{
+    ch=ph$coalhist
+
+    pops=ph$pophist
+
+    ch=merge(ch,pops,by.x="src",by.y="pop")[,c(-6,-7)]
+    ch=ch[with(ch,order(time,src,snk)),]
+    ch=rbind(data.frame(src=pops$pop[(!is.na(pops$arrive))&(pops$arrive==0)],
+                        time=pops$arrive[(!is.na(pops$arrive))&(pops$arrive==0)],
+                        snk=pops$source[(!is.na(pops$arrive))&(pops$arrive==0)],
+                        col=pops$col[(!is.na(pops$arrive))&(pops$arrive==0)],
+                        row=pops$row[(!is.na(pops$arrive))&(pops$arrive==0)]),
+             ch)
+                        
+    
+    layout(matrix(c(1,1,1,1,2,3),nrow=3,ncol=2,byrow=T))
+    rows <- min(pops$row):max(pops$row)
+    cols <- min(pops$col):max(pops$col)
+    ch$coldist=NA
+    plot(1,type="n",ylab="",xlab="",ylim=range(rows),xlim=range(cols),axes=F,asp=1)
+    rect(par("usr")[1],par("usr")[3],par("usr")[2],par("usr")[4],col = "gray") #gray background
+    points(row~col,pops,pch=16,cex=log(dim(pops)[1])/6)
+    for (i in 1:dim(ch)[1])
+    {
+            x1=ch[i,"col"]
+            y1=ch[i,"row"]
+            x0= ch[ch$src==ch[i,"snk"],"col"]
+            y0= ch[ch$src==ch[i,"snk"],"row"]
+#            print(c(x0,y0,x1,y1))
+            arrows(x0,y0,x1,y1,
+                   col=heat.colors(max(ch$time,na.rm=T))[ch$time[i]],
+                   lwd=2,
+                   length=0.1
+               )
+            ch$coldist[i] <- sqrt((y0-y1)^2 + (x0-x1)^2)
+        
+    }
+
+    hist(ch$coldist,xlab="Colonization distance",main="")
+    hist(ch$arrive,xlab="Colonization time",main="")
+    layout(matrix(1))
+}
+
+
+#'
+#' plot the population history
 #' @param pops population history
 #' @export
 #' 
-plothist <- function(pops)
+plothist.old <- function(ph)
 {
+    pops=ph$pophist
+    
     layout(matrix(c(1,1,1,1,2,3),nrow=3,ncol=2,byrow=T))
     rows <- min(pops$row):max(pops$row)
     cols <- min(pops$col):max(pops$col)
