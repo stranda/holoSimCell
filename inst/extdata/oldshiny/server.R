e8042a6179b823e873c49fa389563a8f1e5fdf3a
allan strand <stranda@gmail.com>
2020-02-07 12:31:16 -0500

    keeping old versions of frontend code

---
+++ inst/extdata/oldshiny/server.R
@@ -0,0 +1,107 @@
+require(holoSimCell)
+require(shiny)
+
+# Define UI for application that draws a histogram
+
+# Define server logic required to draw a histogram
+server <- function(input, output,session) {
+
+    habgrid <- reactive({
+        if (input$usehab=="Pollen")
+        {
+            ashpollen
+        } else {
+            ashenm
+        }})
+
+    observeEvent(input$usehab,{
+        hg <- habgrid()
+        details <- hg$details
+        updateNumericInput(session,"ref1",max=details[1,"ncells"])
+        updateNumericInput(session,"ref2",max=details[1,"ncells"])
+        updateNumericInput(session,"xdim",value=details[1,"x.dim"])
+        updateNumericInput(session,"ydim",value=details[1,"y.dim"])
+        updateNumericInput(session,"gens",value=dim(hg$hab_suit)[1])
+    })
+    
+    output$dispkern <- renderPlot({
+        thresh = 1e-9
+        dom=0:100000
+        dens <- distancePDF(dom,ssh=input$ssh,ssc=(input$ssc/100)*input$sz,
+                            lmn=(input$nmn/100)*input$sz,
+                            lsd=sqrt(input$nmn),mix=input$mix)
+        nonzero.dens <- dens[1:max(which(dens>thresh))]
+        nonzero.dom <- dom[1:max(which(dens>thresh))]
+        prop.dom = nonzero.dom
+        plot(nonzero.dens ~ prop.dom,
+             type="l", xlab="Dispersal distance in same units as cell width",
+             ylab="Density")
+        text(0.9*max(nonzero.dom),0.9*max(nonzero.dens),paste("Only densities >",thresh,"shown"))
+    })
+    
+    output$histplot <- renderPlot({
+        input$doplot
+        isolate({
+            if ((input$ref1>0)&(input$ref2>0))
+            {
+                refs=c(input$ref1,input$ref2)
+                refsz=c(input$ref1size,input$ref2size)
+            } else {
+                if (input$ref1>0)
+                {
+                    refs=c(input$ref1)
+                    refsz=c(input$ref1size)
+                } else {
+                    refs=c(input$ref2)
+                    refsz=c(input$ref2size)
+                }
+                
+            }
+
+            if (input$usehab!="None") hs=habgrid() else hs=NULL
+            
+            ph <- getpophist.cells(h=input$xdim*input$ydim,          ##demography, num habitats
+                                         xdim=input$xdim,        ##num cols
+                                         ydim=input$ydim,        ##num rows
+                                         maxtime=input$gens,  ##num time clicks to simulate (yrs, decades cents?) (call em decades here)
+                                         lambda=input$lambda, ##intrisic rate of growth (discrete-time...right? can be modified with deltLambda)
+                                        #deltLambda: see function definition
+                                         K=input$K,        ##carry capacity (can be modified with deltK)
+                                        #deltK: see function definition
+                                         refs=refs,    ##pop ids of refugia
+                                         refsz=refsz, ##sizes of refugia indicated in refs
+                                         sz=input$sz,   ##number of spatial units per grid cell
+                                        #dispersal traits
+                                         distance.fun=distancePDF, #takes a length and 5 parameters and creates mig matrix
+                                         shortscale=(input$ssc/100)*input$sz, #scale of short distance weibull
+                                         longmean=(input$nmn/100)*input$sz,      #mean of long-distance norm (var is the same as the mean)
+                                         shortshape=input$ssh,    #shape of short-distance
+                                         mix=input$mix,         #proportion of LDD
+                                         popDispInfl=function(x){log(x+1)},
+                                         samptime = 1,  #!# sample every X generations
+                                         CVn = NULL,       #!# coefficient of variation X% 
+                                         pois.var = FALSE, #!# Poisson distribution for demographic stochasticity
+                                         extFUN = NULL,
+                                         hab_suit = hs
+                                       )
+#            print(ph$coalhist)
+            print("about to plot")
+            if (sum(!is.na(ph$pophist$source))>1)
+            {
+                if (input$sc2mxTime) mxt = input$gens else mxt= NULL 
+                plothist(ph,maxtime=mxt)
+            }
+            else
+            {
+                plot(1~1,xlab="",ylab="",type="n")
+                text(1,1,"No populations colonized!" )
+            }
+            print("plotting done")
+            
+        })
+   })
+}
+
+# Run the application 
+#shinyApp(ui = ui, server = server)
+
---
+++ inst/extdata/oldshiny/ui.R
@@ -0,0 +1,77 @@
+require(shiny)
+require(holoSimCell)
+##
+ui <- fluidPage(
+   
+   # Application title
+   titlePanel("Forward-time simulation visualizer"),
+   
+   # Sidebar with a slider input for number of bins 
+   sidebarLayout(
+      sidebarPanel(
+ 
+          actionButton("doplot","Run simulation and plot"),
+         sliderInput("ssc",
+                     "Scale of short-dist (percent of cell width)",
+                     min = 0,
+                     max = 200,
+                     value = 4),
+         sliderInput("ssh",
+                     "Shape of short-dist",
+                     min = 0.01,
+                     max = 10,
+                     value = 1),
+         sliderInput("nmn",
+                     "Scale of long-dist (percent cell width)",
+                     min = 0,
+                     max = 400,
+                     value = 35),
+         sliderInput("mix",
+                     "proportion of long-dist",
+                     min = 0,
+                     max = 0.1,
+                     value = 0.001),
+         sliderInput("sz",
+                     "Number of measurement units across cells",
+                     min = 1,
+                     max = 2000,
+                     value = 150),
+         numericInput("lambda","lambda",value=1.01,min=0.5,max=1.5),
+
+         sliderInput("K",
+                     "carry capacity",
+                     min=10,max=5000,value=500),
+         sliderInput("gens",
+                     "Number of time clicks:",
+                     min = 1,
+                     max = 10000,
+                     value = 500),
+
+         checkboxInput("sc2mxTime","Scale colors to full simulation period",FALSE),
+
+         radioButtons("usehab","Which habitat suitability",c("None","ENM","Pollen")),
+         
+         numericInput("xdim","number of X grids",value=15,min=1,max=100),
+         
+         numericInput("ydim","number of Y grids",value=15,min=1,max=100),
+         
+         numericInput("ref1","Refugium pop num",value=10),
+
+         numericInput("ref1size","Refugium pop size",value=100),
+         
+         numericInput("ref2","Refugium pop num",value=0),
+         
+         numericInput("ref2size","Refugium pop size",value=0)
+         
+      ),
+      
+      # Show a plot of the generated distribution
+      mainPanel(
+          h3("Current dispersal kernel"),
+          plotOutput("dispkern"),
+          h3("Map of colonization history. Red oldest, white most recent"),
+         plotOutput("histplot",height="900px")
+      )
+   )
+)
+
